e4ecfcc12c95b23e5df3b19836c37d80752b0c95
akucherevskiy <a.kucherevskiy@gmail.com>
2015-05-07 21:34:11 +0300

    add test_slip

--- main.py
+++ main.py
@@ -32,13 +32,13 @@ class Game:
         global elements 
         elements = [[Point(i,j,0) for i in range(range_matrix)] for j in range(range_matrix)]   
     
-    def print_matrix(self):
+    def print_matrix(self, col_count = range_matrix):
         for x1 in range(range_matrix):
-	    for y1 in range(range_matrix):
+	    for y1 in range(col_count):
 	        sys.stdout.write(" %s " %  elements[x1][y1].value)	    
 	    print
 
-    def check_if_lose(self):
+    def check_if_lose(self): #to test
         zeroes = 0
         for x1 in range(range_matrix):
 	    for y1 in range(range_matrix):
@@ -57,7 +57,7 @@ class Game:
             print 'Invalid arg!'
             return
 
-    def slip(self, side, elem):
+    def slip(self, side, elem): #to test
         slip_count = 1
         add_new_point_flag = 0        
         while slip_count > 0:
@@ -69,7 +69,7 @@ class Game:
 		    add_new_point_flag +=1
 	    count = 0 
             for i in range(len(elem)-1):
-                if elem[self.side_point(side, i)[0]].value == 0 and elem[self.side_point(side, i)[1]].value > elem[i].value:
+                if elem[self.side_point(side, i)[0]].value == 0 and elem[self.side_point(side, i)[1]].value > elem[self.side_point(side, i)[0]].value:
 	            count += 1
             slip_count = count if count > 0 else 0
         return add_new_point_flag
@@ -156,40 +156,82 @@ class Game:
                 print 'Only a w s d allowed!\n'
 
 class TestCase(Game):
-    def throw_error(self,func_name):
-        return 'error in ' + func_name + '!'
+    
+    def __init__(self, errors_count = 0):
+        self.errors_count = errors_count
 
+    def return_error(self,func_name):
+        return '\033[91merror in ' + func_name + '! \033[0m'
+
+    def return_success(self,func_name):
+	return '\033[92msuccess in ' + func_name + '\033[0m'
+    
     def test_fill_matrix(self):
         self.fill_matrix()
-        print 'test_fill_matrix:'
-        
 	for x in range(range_matrix):
 	    for y in range(range_matrix):
-	        print True if isinstance(elements[x][y], Point) else self.throw_error('test_fill_matrix\n')
+	        if isinstance(elements[x][y], Point):
+		    print self.return_success('test_fill_matrix')		
+		else:
+		    print self.return_error('test_fill_matrix\n')
+		    self.errors_count += 1
 
     def test_add_random_points(self):
 	last_count = 0
 	self.fill_matrix()
 	for k in [1,2]:
-	    print 'test_add_random_points - ', 'with matrix update' if k==2 else 'without matrix update\n', 
+	    #print 'test_add_random_points - ', 'with matrix update\n' if k==2 else 'without matrix update\n', 
 	    for i in range(1, 5):
 	        tmp = 0 if k == 1 else self.fill_matrix() 
 	        self.add_random_points(i)
-	        print i, ' points:'
 	        count = 0
 		
 	        for x in range(range_matrix):
 	            for y in range(range_matrix):
 	                count += 1 if elements[x][y].value > 0 else 0
 	        
-	        print True if count == (last_count+1 if k == 1 else i)  else self.throw_error('test_add_random_points')
+	        if count == (last_count+1 if k == 1 else i):
+		    print self.return_success('test_add_random_points') 		
+		else:
+		    print self.return_error('test_add_random_points')
+		    self.errors_count += 1
 	        last_count =  count + i if k == 1 else 0
-
+    def test_slip(self):
+	global elements
+	self.fill_matrix()
+	col_ar = [ [[0,0,0,0],[0,0,0,0]],
+		   [[0,0,0,1],[0,0,0,1]],
+		   [[0,0,1,0],[0,0,0,1]],
+		   [[0,0,1,1],[0,0,1,1]],
+		   [[0,1,0,0],[0,0,0,1]],
+		   [[0,1,0,1],[0,0,1,1]],
+		   [[0,1,1,0],[0,0,1,1]],
+		   [[0,1,1,1],[0,1,1,1]],
+		   [[1,0,0,0],[0,0,0,1]],
+		   [[1,0,0,1],[0,0,1,1]],
+		   [[1,0,1,0],[0,0,1,1]],
+		   [[1,0,1,1],[0,1,1,1]],
+		   [[1,1,0,0],[0,0,1,1]],
+		   [[1,1,0,1],[0,1,1,1]],
+		   [[1,1,1,0],[0,1,1,1]],
+		   [[1,1,1,1],[1,1,1,1]]]
+	for y in range(0,4):	
+	    for i in range(0, 16):
+                for x in range(0,4):
+    		    elements[x][y].value = col_ar[i][0][x]
+	        self.slip('s', [elements[0][y],elements[1][y],elements[2][y],elements[3][y]])
+ 	        for x in range(0,4):
+		    if elements[x][y].value == col_ar[i][1][x]:
+		        print self.return_success('test_slip')
+		    else:
+			print self.return_error('test_slip')
+			self.errors_count +=1
 test_obj = TestCase()
 test_obj.test_fill_matrix()
 test_obj.test_add_random_points()
-
-game = Game()
-game.init()
-game.main()
+test_obj.test_slip()
+print '\033[94mTotal errors - ', test_obj.errors_count, '\033[0m'
+#game = Game()
+#game.init()
+#game.main()
 
